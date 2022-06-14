6e5845fe5a0995678177252cba3395f65b1b0e04
mari2654 <mariana.santos2654@gmailcom>
2022-06-14 09:03:28 -0300

    finalizado

---
+++ script.js
@@ -0,0 +1,24 @@
+for(let number=10; number>=0; number--){
+    console.log("repetição com o for loop:number");
+}
+
+let number=10;
+
+while(number>=0){
+    console.log("repetição com o while:number");
+      number--;
+}
+
+for(let number=1; number<=100; number++){
+  console.log("número ímpar:number");
+    if(number %2===0){
+      console.log(number);
+    }
+}
+
+for(let number=0; number<=100; number++){
+    console.log("número par:number");
+        if(number %2===0){
+            console.log(number);
+        }
+}
